e1d77eb36082b24409ba596607bad8b768c27a0e
zidane.nur004 <zidane.nur004@gmail.com>
2025-07-19 02:13:06 +0000

    progres admin dashboard

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,89 @@
+import { IconType } from "react-icons";
+import { FaRegUser } from "react-icons/fa";
+import { GoPackage } from "react-icons/go";
+import { IoMdAdd } from "react-icons/io";
+import { MdAttachMoney } from "react-icons/md";
+
 export default function Page() {
+    type StatType = {
+        icon: IconType,
+        iconColors: string,
+        title: string,
+        value: string | number
+    }
+
+    const stats: StatType[] = [
+        {
+            icon: GoPackage,
+            iconColors: "bg-purple-900",
+            title: "Total Products",
+            value: 16
+        },
+        {
+            icon: MdAttachMoney,
+            iconColors: "bg-green-700",
+            title: "Total Revenue",
+            value: 100000
+        },
+        {
+            icon: FaRegUser,
+            iconColors: "bg-blue-800",
+            title: "Total Users Active",
+            value: 160
+        }
+    ];
+
     return (
         <main className="min-h-screen flex justify-center bg-gradient-to-b from-purple-950 to-black relative">
             <div className="absolute inset-0 bg-black/50"></div>
-            <div className="relative mt-30 flex flex-col w-7xl">
-                <div className="w-full flex justify-between items-center">
-                    <div className="h1 text-3xl font-bold">Admin Dashboard</div>
+            <div className="relative mt-30 flex flex-col gap-y-6 w-7xl">
+                {/* Header */}
+                <section className="w-full flex justify-between items-center">
+                    <div className="flex flex-col gap-y-2">
+                        <h1 className="text-3xl font-bold">Admin Dashboard</h1>
+                        <p className="text-slate-300">Manage products and monitor store performance</p>
+                    </div>
+                    <div className="flex gap-x-3 items-center">
+                        <button className="py-2 px-4 rounded-md bg-purple-900 cursor-pointer flex gap-x-2 items-center hover:bg-purple-700 duration-300">
+                            <IoMdAdd className="size-6" />
+                            <p>Add Game</p>
+                        </button>
+                        <button className="py-2 px-4 rounded-md bg-purple-900 cursor-pointer flex gap-x-2 items-center hover:bg-purple-700 duration-300">
+                            <IoMdAdd className="size-6" />
+                            <p>Add Product</p>
+                        </button>
+                    </div>
+                </section>
+
+                {/* Stat */}
+                <section className="grid grid-cols-3 w-full gap-x-4">
+                    {stats.map((stat, id) => (
+                        <div key={id} className="bg-gradient-to-b from-white/5 to-purple-950/60 border border-purple-950/60 rounded-md px-6 py-4 flex gap-x-4 items-center shadow-xl">
+                            <div className={`flex justify-center items-center p-4 rounded-md ${stat.iconColors}`}>
+                                <stat.icon className="size-6" />
+                            </div>
+                            <div className="flex justify-between flex-col">
+                                <p className="text-slate-300">{stat.title}</p>
+                                <p className="text-xl font-bold">{stat.value}</p>
+                            </div>
+                        </div>
+                    ))}
+                </section>
+
+                {/* Products */}
+                <div className="flex flex-col gap-y-4 bg-white/5 px-6 py-4 rounded-xl w-full">
+                    <table className="w-full">
+                        <thead>
+                            <tr className="border-b border-slate-400 text-slate-400">
+                                <td className="p-4 text-left">Product</td>
+                                <td className="p-4 text-left">Game</td>
+                                <td className="p-4 text-left">Price</td>
+                                <td className="p-4 text-left">Stock</td>
+                                <td className="p-4 text-left">Status</td>
+                                <td className="p-4 text-left">Actions</td>
+                            </tr>
+                        </thead>
+                    </table>
                 </div>
             </div>
         </main>
